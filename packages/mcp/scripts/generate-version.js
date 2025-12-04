e52b6e9938507b1321d653ad28a0b389b53675a0
Baptiste Manach <b.manach@theseus-ai.com>
2025-12-04 13:21:53 +0100

    chore(mcp): update generate-version.js to include full Apache License 2.0 header
    - Added complete Apache License 2.0 header to the generated version file
    - Ensured the file is marked as auto-generated and should not be edited directly

--- packages/mcp/scripts/generate-version.js
+++ packages/mcp/scripts/generate-version.js
@@ -1,6 +1,6 @@
 /*
  * Copyright 2025 BigRack.dev
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,20 @@ const packageJsonPath = path.join(__dirname, '../package.json');
 const versionFilePath = path.join(__dirname, '../src/version.ts');
 
 const packageJson = require(packageJsonPath);
-const content = `// This file is generated automatically. Do not edit.
+const content = `/*
+ * Copyright 2025 BigRack.dev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// This file is generated automatically. Do not edit.
 export const version = '${packageJson.version}';
 `;
 
